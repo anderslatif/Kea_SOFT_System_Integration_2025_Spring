3d7881a9376e423fd278964d5b0665efe7e47f23
anderslatif <anderslatif@users.noreply.github.com>
2025-02-13 12:19:46 +0100

    Finished the dotenv example for Node.js

--- 09._Environment_Variables/02._python/main.py
+++ 09._Environment_Variables/02._python/main.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv, dotenv_values
 import os
 
+
 load_dotenv()
-print(os.getenv("API_KEY"))
+# print(os.getenv("API_KEY"))
 
 config = dotenv_values(".env")
-print(config["API_KEY"])
+# print(config["API_KEY"])
+
+print(os.environ)
